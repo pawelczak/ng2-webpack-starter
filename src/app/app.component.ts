d688e04fa4a4f21122eace242f3b9946236512b3
Łukasz Pawełczak <lukaspawelczak@gmail.com>
2016-08-12 08:51:58 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+// 3d party imports
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app',
+    template: ``
+})
+export class AppComponent {}
---
+++ src/main.browser.ts
@@ -0,0 +1,9 @@
+// 3d party imports
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+// app imports
+import { AppComponent } from './app/app.component';
+
+bootstrap(AppComponent, [
+
+]).catch(err => console.error(err));
