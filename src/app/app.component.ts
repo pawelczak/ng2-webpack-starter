bdc1c1c2eecebbd8eb1dd5a00eae6118f73b3c1e
Łukasz Pawełczak <lukaspawelczak@gmail.com>
2016-10-16 23:26:05 +0200

    Angular2 AoT play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    templateUrl: './app.component.html'
+    // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
